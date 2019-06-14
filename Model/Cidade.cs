7a4125849f160abe40cbb482f35c278c2ba04bd4
YagoGameiro <48812787+YagoGameiro@users.noreply.github.com>
2019-06-13 21:56:08 -0300

    Add files via upload

---
+++ Model/Cidade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Cidade
+    {
+        #region Propriedades
+
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int IdEstado { get; set; }
+        public int IdPais { get; set; }
+        #endregion
+    }
+}
---
+++ Model/Estado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Estado
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public int IdPais { get; set; }
+
+    }
+}
