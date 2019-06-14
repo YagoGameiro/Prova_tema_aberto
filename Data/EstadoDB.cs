aca754247c6ffff1c9740c189845f622444ca8d1
YagoGameiro <48812787+YagoGameiro@users.noreply.github.com>
2019-06-13 21:55:07 -0300

    Add files via upload

---
+++ Data/EstadoDB.cs
@@ -0,0 +1,43 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util;
+
+namespace Data
+{
+    public class EstadoDB
+    {
+        private Conexao conexao;
+
+        public List<Estado> All(int id)
+        {
+            using (conexao = new Conexao())
+            {
+                var sql = "SELECT Id, Descricao FROM TB_Estado WHERE IdPais = " + id;
+                var retorno = conexao.ExecutaComandoRetorno(sql);
+                return TransformaSQLReaderEmList(retorno);
+            }
+        }
+
+        private List<Estado> TransformaSQLReaderEmList(SqlDataReader retorno)
+        {
+            var listEstado = new List<Estado>();
+
+            while (retorno.Read())
+            {
+                var item = new Estado()
+                {
+                    Id = Convert.ToInt32(retorno["Id"]),
+                    Descricao = retorno["Descricao"].ToString()
+                };
+
+                listEstado.Add(item);
+            }
+            return listEstado;
+        }
+    }
+}
---
+++ Data/PaisDB.cs
@@ -0,0 +1,43 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util;
+
+namespace Data
+{
+    public class PaisDB
+    {
+        private Conexao conexao;
+
+        public List<Pais> All()
+        {
+            using (conexao = new Conexao())
+            {
+                var sql = "SELECT Id, Descricao FROM TB_Pais";
+                var retorno = conexao.ExecutaComandoRetorno(sql);
+                return TransformaSQLReaderEmList(retorno);
+            }
+        }
+
+        private List<Pais> TransformaSQLReaderEmList(SqlDataReader retorno)
+        {
+            var listPais = new List<Pais>();
+
+            while (retorno.Read())
+            {
+                var item = new Pais()
+                {
+                    Id = Convert.ToInt32(retorno["Id"]),
+                    Descricao = retorno["Descricao"].ToString()
+                };
+
+                listPais.Add(item);
+            }
+            return listPais;
+        }
+    }
+}
