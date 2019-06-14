3ac7258a896f9af31ea42e453ac0e8c4c7c578ac
YagoGameiro <48812787+YagoGameiro@users.noreply.github.com>
2019-06-13 21:58:03 -0300

    Add files via upload

---
+++ ProvaTemaAberto/Default.aspx.cs
@@ -0,0 +1,63 @@
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProvaTemaAberto
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadPais();
+                LoadEstado(1);
+                LoadCidade(1, 1);
+            }
+            
+        }
+
+        private void LoadCidade(int idE, int idP)
+        {
+            list_cidade.DataSource = new CidadeDB().All(idE, idP);
+            list_cidade.DataTextField = "Descricao";
+            list_cidade.DataValueField = "Id";
+            list_cidade.DataBind();
+        }
+
+        protected void list_pais_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadEstado(int.Parse(list_pais.SelectedValue));
+        }
+        protected void list_estado_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadCidade(int.Parse(list_estado.SelectedValue), int.Parse(list_pais.SelectedValue));
+        }
+
+        private void LoadEstado(int id)
+        {
+            list_estado.DataSource = new EstadoDB().All(id);
+            list_estado.DataTextField = "Descricao";
+            list_estado.DataValueField = "Id";
+            list_estado.DataBind();
+        }
+
+        private void LoadPais()
+        {
+            list_pais.DataSource = new PaisDB().All();
+            list_pais.DataTextField = "Descricao";
+            list_pais.DataValueField = "Id";
+            list_pais.DataBind();
+        }
+
+        protected void list_cidade_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
---
+++ ProvaTemaAberto/Default.aspx.designer.cs
@@ -0,0 +1,60 @@
+﻿//------------------------------------------------------------------------------
+// <gerado automaticamente>
+//     Este código foi gerado por uma ferramenta.
+//
+//     As alterações ao arquivo poderão causar comportamento incorreto e serão perdidas se
+//     o código for recriado
+// </gerado automaticamente>
+//------------------------------------------------------------------------------
+
+namespace ProvaTemaAberto {
+    
+    
+    public partial class Default {
+        
+        /// <summary>
+        /// Controle form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Controle list_pais.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList list_pais;
+        
+        /// <summary>
+        /// Controle list_estado.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList list_estado;
+        
+        /// <summary>
+        /// Controle list_cidade.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList list_cidade;
+        
+        /// <summary>
+        /// Controle gv_total.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gv_total;
+    }
+}
